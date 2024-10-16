754193d7fec4b148bcb22fb36078d2d81876a333
dvj5411 <dvj5411@psu.edu>
2024-10-16 14:24:41 -0400

    barebones (very lol)

--- fetcher.py
+++ fetcher.py
@@ -1,6 +1,6 @@
 import urllib.request, json
 
-urlLink = ""
-with urllib.request.urlopen() as url:
+urlLink = "https://esi.evetech.net/v1/killmails/120569227/bef1a45562d05559443646a53e35f3f6a507027d/?datasource=tranquility"
+with urllib.request.urlopen(urlLink) as url:
     data = json.load(url)
     print(data)
